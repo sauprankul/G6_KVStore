95565d6a46bfe030c1178ea53e83889b8cb33adb
Saurabh Kulkarni <saurabh.kulkarni997@gmail.com>
2021-02-26 10:02:03 -0600

    added script

--- startKV2.sh
+++ startKV2.sh
@@ -9,7 +9,7 @@ while [[ counter -lt $1 ]]
 do
     port=50000
     port=$(($port + $counter))
-    java -jar KVServer2_0.jar $port
+    java -jar KVServer2_0.jar $port &
     counter=$(($counter + 1))
 done
 
