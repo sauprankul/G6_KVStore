56cb029e7f9382241edb4016f5a1fd24cf8c9a0b
Saurabh Kulkarni <saurabh.kulkarni997@gmail.com>
2021-02-26 09:58:33 -0600

    draft of script

---
+++ startKV2.sh
@@ -0,0 +1,16 @@
+if [[ $# -eq 0 ]]
+    then
+        echo "Need argument n (int)"
+        return
+fi
+
+counter=0
+while [[ counter -lt $1 ]]
+do
+    port=50000
+    port=$(($port + $counter))
+    java -jar KVServer2_0.jar $port
+    counter=$(($counter + 1))
+done
+
+echo "Script done"
