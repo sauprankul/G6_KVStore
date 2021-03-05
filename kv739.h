07cae862d253d22ffd1ad00dba0c1fa7d5830cbb
rami <ramidahman27@gmail.com>
2021-03-04 21:38:30 -0600

    frontend: adding library and updated header

--- kv739.h
+++ kv739.h
@@ -2,10 +2,12 @@
 extern "C" {
 #endif
 
-int kv739_init(char *server_name); 
+int kv739_init(char **server_name); 
 int kv739_shutdown(void); 
 int kv739_get(char* key, char* value); 
 int kv739_put(char* key, char* value, char* old_value); 
+int kv739_die(char* server_name, int clean);
+int kv739_partition(char* server_name, char** reachable);
 
 #ifdef __cplusplus
 }
