f01e585e31486dfdaf0df5e872c8b2652943e7f6
Saurabh Kulkarni <saurabh.kulkarni997@gmail.com>
2021-03-08 01:08:28 -0600

    Fixed partition update issue

--- startKV2.sh
+++ startKV2.sh
@@ -6,7 +6,7 @@ fi
 
 counter=0
 lines=$(wc -l < "$1")
-echo "$lines" "lines detected. (please check that there is a newline at the end of the config file"
+echo "$lines" "lines detected. If this is not right, please check that there is a newline at the end of the config file."
 while [[ counter -lt lines ]]
 do
     echo $counter
